6712b847aff869274ba501e3c3b94c9558e3c7d6
bobocodex57 <bobo.codex@gmail.com>
2020-01-05 21:03:56 +0630

    feature/bobo57 - db:seed, default role

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -3,7 +3,6 @@
 use App\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Spatie\Permission\Models\Role;
 
 class UsersTableSeeder extends Seeder
 {
@@ -14,16 +13,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = DB::table('users')->where('name', 'codex')->get();
-
-        if($user){
-            $user = factory(\App\User::class, 5)->create();
-        }
-        else {
-            $user = factory(\App\User::class)->create();
-            $role = Role::where('name', '=', 'Admin')->firstOrFail();
-            $user->roles()->attach($role);
-        }
-        
+        factory(\App\User::class)->create();   
     }
 }
